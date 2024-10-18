5818ac0c22701fac19e03715f5962529ac778182
DiegoDISB <109098115+DiegoDISB@users.noreply.github.com>
2024-10-17 23:04:48 -0500

    Cambio por DIego Salazar Badillo (#3)
    Co-authored-by: Diego Salazar Badillo <diego.salazar@megaprofer.com>

--- main.py
+++ main.py
@@ -46,4 +46,6 @@ if __name__ == "__main__":
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
+    print("Diego Salazar Badillo")
+
     print(sort_list(word_list))
